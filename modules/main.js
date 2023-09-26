c47534a52c6278629123872ed01308a1bfa95cea
Shubham Deshmukh <Shubhamdeshm37@gmail.com>
2023-09-26 20:41:26 +0530

    Modules - Advance js

---
+++ modules/main.js
@@ -0,0 +1,10 @@
+// import {message, user as us, test} from './library.js';
+import * as dsk from './library.js';
+console.log (message);
+console.log (dsk.message);
+// user("shubham");
+// console.log (user ('shubham'));
+console.log (dsk.user ('shubham'));
+console.log (us ('shubham'));
+let a = new test ();
+let b = new dsk.test ();
